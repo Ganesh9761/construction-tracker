e487e642b215bd102399714c19547c518dad434a
kali <kali@kali.kali>
2026-09-17 09:44:08 -0400

    feat: initialize Flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 
 from config import Config
+from app.extensions import csrf, db, login_manager, migrate
 
 
 def create_app(config_class=Config):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    csrf.init_app(app)
 
     return app
