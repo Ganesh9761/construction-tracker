ad86d521d8303179bcf29953602f49637de73d75
kali <kali@kali.kali>
2026-09-18 03:59:07 -0400

    chore: register expense model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
+from app.models.expense import Expense
 from app.models.phase import Phase
 from app.models.project import Project
 from app.models.user import User
 
-__all__ = ["Phase", "Project", "User"]
+__all__ = ["Expense", "Phase", "Project", "User"]
